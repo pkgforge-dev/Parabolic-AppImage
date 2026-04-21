6f299009e30f7b84fbaa0562c24b9c426a7dcf56
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-20 23:32:16 -0400

    fix?

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm python-websockets
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
